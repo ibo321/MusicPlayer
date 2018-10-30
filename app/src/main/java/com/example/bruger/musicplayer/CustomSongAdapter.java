9e00693ac3f3e4307dc88b0fe42494f11fb0a199
ibo321 <iboo96_96@hotmail.com>
2018-10-30 20:22:48 +0100

    Initial commit

---
+++ app/src/main/java/com/example/bruger/musicplayer/CustomSongAdapter.java
@@ -0,0 +1,103 @@
+package com.example.bruger.musicplayer;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class CustomSongAdapter extends BaseAdapter {
+
+    public CustomSongAdapter(Context context, int layout, ArrayList<Song> arrayList) {
+        this.context = context;
+        this.layout = layout;
+        this.arrayList = arrayList;
+    }
+
+    private Context context;
+    private int layout;
+    private ArrayList<Song> arrayList;
+    private MediaPlayer mediaPlayer;
+    private Boolean flag = true;
+
+    @Override
+    public int getCount() {
+        return arrayList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    private class ViewHolder{
+        TextView txtName, txtSinger;
+        ImageView playB, stopB;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        final ViewHolder viewHolder;
+        if (convertView == null){
+            viewHolder = new ViewHolder();
+
+            LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = layoutInflater.inflate(layout, null);
+
+            viewHolder.txtName = (TextView) convertView.findViewById(R.id.songName_text);
+            viewHolder.txtSinger = (TextView) convertView.findViewById(R.id.singer_text);
+            viewHolder.playB = (ImageView) convertView.findViewById(R.id.play_png);
+            viewHolder.stopB = (ImageView) convertView.findViewById(R.id.stop_png);
+
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+        final Song song = arrayList.get(position);
+
+        viewHolder.txtName.setText(song.getName());
+        viewHolder.txtSinger.setText(song.getSinger());
+
+        // play music
+        viewHolder.playB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(flag){
+                    mediaPlayer = MediaPlayer.create(context, song.getSong());
+                    flag = false;
+                }
+                if(mediaPlayer.isPlaying()) {
+                    mediaPlayer.pause();
+                    viewHolder.playB.setImageResource(R.drawable.play);
+                } else {
+                    mediaPlayer.start();
+                    viewHolder.playB.setImageResource(R.drawable.pause);
+                }
+            }
+        });
+
+        // stop
+        viewHolder.stopB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!flag) {
+                    mediaPlayer.stop();
+                    mediaPlayer.release();
+                    flag = true;
+                }
+                viewHolder.playB.setImageResource(R.drawable.play);
+            }
+        });
+        return convertView;
+    }
+}
---
+++ app/src/main/java/com/example/bruger/musicplayer/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.bruger.musicplayer;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Song> arrayList;
+    private CustomSongAdapter adapter;
+    private ListView songList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //get listView
+        songList = (ListView) findViewById(R.id.song_ListView);
+
+        //songs
+        arrayList = new ArrayList<>();
+        arrayList.add(new Song("- Formation","Beyoncé",R.raw.beyonce_formation));
+        arrayList.add(new Song("- Hope You Do","Chris Brown",R.raw.chrisbrown_hopeyoudo));
+        arrayList.add(new Song("- Beautiful","Akon ft. Colby'O'Donis",R.raw.akon_beautiful_ft_colbyodonis_kardinaloffishall));
+        arrayList.add(new Song("- Don't Matter", "Akon", R.raw.akon_dontmatter));
+        arrayList.add(new Song("- Locked Up", "Akon", R.raw.akon_lockedup_ft_stylesp));
+        arrayList.add(new Song("- Sweet but Psycho", "Ava Max", R.raw.avamax_sweetbutpsycho));
+        arrayList.add(new Song("- Ghetto","Tupac and Biggie ft. Akon Remix", R.raw.tupacbiggieakon_ghetto));
+        adapter = new CustomSongAdapter(this, R.layout.custom_song_item, arrayList);
+        songList.setAdapter(adapter);
+    }
+}
